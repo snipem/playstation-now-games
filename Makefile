baf46885b526018aec38e6f0b79ad1330403fe5d
Matthias Küch <mail@matthias-kuech.de>
2021-06-12 21:34:22 +0200

    Fetch pup as binary

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 html:
 	./get-ps-now-games.sh | sed 's/^/. /g' | asciidoctor -o - - > public/index.html
+
+pup:
+	wget https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip
+	unzip pup_v0.4.0_linux_amd64.zip
--- get-ps-now-games.sh
+++ get-ps-now-games.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # run: ./get-ps-now-games.sh
+set -x
+
+# Add current folder to path for pup
+PATH=$PATH:.
 
 curl -s "https://www.playstation.com/en-us/ps-now/ps-now-games/" |
     pup ".tabs__tab-content > div > div > div > div > div > div > p text{}" |
